6d96bf86d19d568ee771831a7d1c2f027f06aae9
akang275 <akang275@gmail.com>
2022-01-10 22:31:54 -0500

    1/10

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 let visualizerContainer = document.querySelector('.visualizer-container');
+let sizeSliderContainer = document.querySelector('.size-slider-container');
+
+let array = [];
+
+function randomizeArray() {
+    let array = [];
+    for (let c; c <) {
+
+    };
+    return array;
+};
+
+for (let c = 0; c < sizeSliderContainer.lastChild().value; c++) {
+    let bar = document.createElement('div');
+    bar.className = 'bar';
+    visualizerContainer.append(bar);
+};
