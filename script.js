87f096eb6376c59e26a49187d6346ca5fda632e4
akang275 <86367191+akang275@users.noreply.github.com>
2022-01-12 13:23:54 -0500

    1/11

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 let visualizerContainer = document.querySelector('.visualizer-container');
 let sizeSliderContainer = document.querySelector('.size-slider-container');
 
-let array = [];
+let arraySize = sizeSliderContainer.lastChild.value;
+
+let array;
 
 function randomizeArray() {
-    let array = [];
-    for (let c; c <) {
+    let range = [];
+    for (let c = 0; c < arraySize; c++) {
+        range.push(c);
+    }
 
+    array = [];
+    for (let c = 0; c < arraySize; c++) {
+        let randomIndex = Math.floor(Math.random() * range.length);
+        let randomNum = range[randomIndex];
+        array.push(randomNum);
+        range.splice(randomIndex, 1);
     };
-    return array;
 };
 
-for (let c = 0; c < sizeSliderContainer.lastChild().value; c++) {
-    let bar = document.createElement('div');
-    bar.className = 'bar';
-    visualizerContainer.append(bar);
-};
+function drawArray() {
+    // clear visualizer container
+    while (visualizerContainer.firstChild) {
+        firstChild.remove();
+    }
+
+    // fill visualizer container
+    for (let c = 0; c < arraySize; c++) {
+        let bar = document.createElement('span');
+        bar.className = 'bar';
+        bar.style.height = array[c]/array.length + '%';
+        visualizerContainer.append(bar);
+    };
+}
+
+randomizeArray();
+drawArray();
