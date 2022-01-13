0fac71d1b30a617814b00c75228d3377df21f2b1
akang275 <86367191+akang275@users.noreply.github.com>
2022-01-13 13:33:25 -0500

    qewfdd

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let visualizerContainer = document.querySelector('.visualizer-container');
 let sizeSliderContainer = document.querySelector('.size-slider-container');
 
 let arraySize = sizeSliderContainer.children[1].value;
-console.log(arraySize);
+
+let barList = visualizerContainer.children;
 
 let array;
 
@@ -12,12 +13,10 @@ function randomizeArray() {
         range.push(c);
     }
 
-    console.log(range);
-
     array = [];
     for (let c = 0; c < arraySize; c++) {
         let randomIndex = Math.floor(Math.random() * range.length);
-        let randomNum = range[randomIndex];
+        let randomNum = range[randomIndex] + 1;
         array.push(randomNum);
         range.splice(randomIndex, 1);
     };
@@ -37,6 +36,15 @@ function drawArray() {
         bar.style.width = 1/array.length * 100 + '%';
         visualizerContainer.append(bar);
     };
+}
+
+// colors
+let focusColor = 'var(--titanium-yellow)';
+
+function selectionSort() {
+    for (let c = 0; c < array.length; c++) {
+        barList[c].style.backgroundColor = focusColor;
+    }
 }
 
 randomizeArray();
