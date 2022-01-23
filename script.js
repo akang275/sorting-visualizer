6e80aa1d9ef515db20b252ede5befb75eada52c1
akang275 <akang275@gmail.com>
2022-01-23 10:49:03 -0500

    dfooeoeo

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-let visualizerContainer = document.querySelector('.visualizer-container');
+//console.log(JSON.parse(JSON.stringify(obj)));
 
-let arraySize = document.querySelector('#size-slider').value;
-let sortingSpeed = document.querySelector('#speed-slider').value;
+let visualizerContainer = document.querySelector('.visualizer-container');
+let selectionSortButton = document.querySelector('#selection-sort-button');
+let randomizeArrayButton = document.querySelector('#randomize-array-button');
+let speedSlider = document.querySelector('#speed-slider');
+let sizeSlider = document.querySelector('#size-slider');
+let stopButton = document.querySelector('#stop-button');
+
+let arraySize;
+let sortingSpeed;
 
 let barList = visualizerContainer.children;
 
 let array = [];
 
 function randomizeArray() {
+    // range is an array to keep track of what numbers have yet to be chosen by random num gen
     let range = [];
     for (let i = 0; i < document.querySelector('#size-slider').value; i++) {
         range.push(i);
     }
 
+    // array is what is actually being sorted
     array = [];
     for (let i = 0; i < document.querySelector('#size-slider').value; i++) {
         let randomIndex = Math.floor(Math.random() * range.length);
@@ -36,75 +45,93 @@ function drawArray() {
         bar.style.width = 1/array.length * 100 + '%';
         visualizerContainer.append(bar);
     };
-}
+};
+
+function updateSpeed() {
+    sortingSpeed = speedSlider.max - speedSlider.value + 5;
+};
+
+function updateSize() {
+    arraySize = sizeSlider.value;
+    randomizeArray();
+    drawArray();
+};
+
+function stopSorting() {
+    for (let i of timeoutArray) {
+        clearTimeout(i);
+    }
+    for (let bar of barList) {
+        bar.style.backgroundColor = ogColor;
+    }
+};
 
 // colors
 let focusColor = 'var(--titanium-yellow)';
 let ogColor = 'var(--spanish-green)';
 
+let timeoutArray = [];
+
 function selectionSort() {
-    /*let i = 0;
-    while (i < array.length) {
-        let j = i + 1;
-        while (j < array.length) {
-            setTimeout(function() {
-                barList[j-1].style.backgroundColor = ogColor
-                barList[j].style.backgroundColor = focusColor;
-                if (array[j] < array[minIndex]) {
-                    minIndex = j;
-                }
-                if (j == array.length - 1) {
-                    barList[i].after(barList[minIndex]);
-                    barList[minIndex].after(barList[i]);
-                    let temp = array[minIndex];
-                    array[minIndex] = array[i];
-                    array[i] = temp;
-                }
-                j += 1;
-            }, 500*j);
-            
-        }
-        i += 1;
-    }*/
     let timer = 0;
-    for (let i = 0; i < array.length; i++) {
+    let storedMinIndex;
+
+    for (let i = 0; i < array.length + 1; i++) {
         
-        if (i > 0) {timer += sortingSpeed * (array.length - i + 1)};
+        if (i > 0) {
+            timer += sortingSpeed * (array.length - i + 2);
+        };
+        timeoutArray.push(setTimeout(function() {
+            if (i > 0) {
+                barList[i-1].style.backgroundColor = ogColor;
+                barList[storedMinIndex].style.backgroundColor = ogColor;
+            };
 
-        setTimeout(function() {
-            let minIndex = i;
-            barList[barList.length - 1].style.backgroundColor = ogColor;
-            if (i > 0) {barList[i-1].style.backgroundColor = ogColor};
-            barList[i].style.backgroundColor = focusColor;
+            if (i < array.length) {
+                let minIndex = i;
+                barList[i].style.backgroundColor = focusColor;
+                for (let j = i + 1; j < array.length + 1; j++) {
+                    timeoutArray.push(setTimeout(function() {
+                        if (j < array.length) {
+                            if (j > i + 1) {barList[j-1].style.backgroundColor = ogColor};
+                            barList[j].style.backgroundColor = focusColor;
+                            if (array[j] < array[minIndex]) {
+                                minIndex = j;
+                            }
+                        }
+                        if (j == array.length) {
+                            barList[j - 1].style.backgroundColor = ogColor;
+                            barList[minIndex].style.backgroundColor = focusColor;
+                            storedMinIndex = minIndex;
 
-            for (let j = i + 1; j < array.length; j++) {
-                setTimeout(function() {
-                    if (j > i + 1) {barList[j-1].style.backgroundColor = ogColor};
-                    barList[j].style.backgroundColor = focusColor;
-                    if (array[j] < array[minIndex]) {
-                        minIndex = j;
-                    }
-                    if (j == array.length - 1) {
-                        barList[i].after(barList[minIndex]);
-                        barList[minIndex].after(barList[i]);
-                        let temp = array[minIndex];
-                        array[minIndex] = array[i];
-                        array[i] = temp;
-                    }
-                }, sortingSpeed * (j - i));
+                            barList[i].after(barList[minIndex]);
+                            barList[minIndex].after(barList[i]);
+                            let temp = array[minIndex];
+                            array[minIndex] = array[i];
+                            array[i] = temp;
+                        }
+                    }, sortingSpeed * (j - i)));
+                }
             }
-        }, timer);
+        }, timer));
     }
 }
 
-let selectionSortButton = document.querySelector('#selection-sort-button');
+speedSlider.addEventListener('input', updateSpeed);
+sizeSlider.addEventListener('input', function() {
+    updateSize();
+    stopSorting();
+});
+
+stopButton.addEventListener('click', stopSorting);
+
 selectionSortButton.addEventListener('click', selectionSort);
 
-let randomizeArrayButton = document.querySelector('#randomize-array-button');
 randomizeArrayButton.addEventListener('click', function() {
+    stopSorting();
     randomizeArray();
     drawArray();
-    });
+});
 
-randomizeArray();
-drawArray();
+updateSpeed();
+updateSize();
